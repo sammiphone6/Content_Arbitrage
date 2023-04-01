567ab2b1ea4d6292b4835a1f758796c463ac6ddf
Sam <sammit@mit.edu>
2023-04-01 21:30:16 +0300

    commits

--- account_adding/experiment.py
+++ account_adding/experiment.py
@@ -1,78 +1,7 @@
 import requests
 from data import open_filedata, instas, save_instas, save_filedata
 import pprint
-# URL = "https://instagram.com/favicon.ico"
-# URL = 'https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/9df63fea7cb5c82ba03664410cd75b30~c5_720x720.jpeg?x-expires=1680465600&amp;x-signature=69u6YVxntijMGQeFTu3WWN%2FmUn8%3D'
-# response = requests.get(URL)
-# open("justinbieber.png", "wb").write(response.content)
 
-# instas = [
-#     ('dorothy7andersonhlh', 'R1ITocGvhD'),
-#     ('nancy6scottafd', 'K6M6Z72Cj2M'),
-#     ('dorothy8collinsmia', 'yDvegpLT8'),
-#     ('donna1johnsonfyu', 'k5gBUiUXMMK'),
-#     ('donna7collinstme', 'HiPq1EvCLRx'),
-#     ('deborah2collinsurb', 'DC0eBUiTH'),
-#     ('sharon5millerwkx', 'nwL7dlwuC'),
-#     ('sarah0wilsonrzt', 'VISXJGOW3'),
-#     ('barbara6turnermnr', '2bBGGLzl'),
-#     ('elizabeth9carterrkf', 'JYCitRo9Tcp'),
-#     ('sharon8bakerecr', 'EaubYVfjj'),
-#     ('margaret5taylorscw', 'JcAEcD6sgqY'),
-#     ('nancy7evansday', 'R7Gv5ZjpqC8'),
-#     ('sharon4wrightdqk', '19HBE5f617'),
-#     ('donna1hernandezmkp', 'VVGN62knB'),
-#     ('carol4hernandezwtz', 'ITeAkCFHmYn'),
-#     ('ruth1clarkkjn', 'UPIBNtjT3'),
-#     ('margaret1robinsonfci', 'q3O4jgPGN'),
-#     ('sarah0millerfaz', 'ltWI6fMwH'),
-#     ('patricia5moorekqx', 'BzYMe0lq'),
-#     ('sarah2milleraqo', '71noXCSU4si'),
-#     ('helen1smithezp', 'vZPMsU872'),
-#     ('linda2edwardswoz', 'UmHSZDSUDsn'),
-#     ('jennifer8harrisevo', 'dFRTkrVnY'),
-#     ('susan3robertsgnm', 'rFaFLZwG'),
-#     ('nancy0carterucv', 'FBcEpbh7B'),
-#     ('nancy3gonzalezsnj', 'SERtHIygEiH'),
-#     ('linda1hernandezdcz', '9agVeoCGeoc'),
-#     ('betty8campbellrud', '8ZvhnPwXv'),
-#     ('sandra7adamsscu', 'RscbbpZFco'),
-#     ('dorothy0davisgbx', 'JE11c8O6Z'),
-#     ('sharon6youngxsv', 'IPFMFvD6KET'),
-#     ('helen9smithndv', 'UELgqRxk8'),
-#     ('barbara6williamswrs', 'VqbHEp4FGk'),
-#     ('sharon2robertsesg', 'tlpBdanYdHb'),
-#     ('sharon9campbellxgn', 'aLZSWp7Bt'),
-#     ('karen8greenelr', 'gj9mLN1pEF'),
-#     ('deborah6jonesjyq', 'zqay1Ksgr'),
-#     ('susan8hernandezlsk', 'j0KTvjhME0'),
-#     ('sandra8clarkylx', '34xCdle7U'),
-#     ('betty6thomasxhz', 'ICEOHJfLzYD'),
-#     ('patricia9jacksondjr', 'vT0ugntAO'),
-#     ('ruth7carterpdu', 'zw8YOh5L67v'),
-#     ('helen2nelsonirl', 'AXlTbY1wH'),
-#     ('maria3hillosz', 'NcLZLhC0Wy'),
-#     ('kimberly2carterscp', 'PiBHXxcdr'),
-#     ('maria6millerjeg', 'NanEFZjM7O'),
-#     ('laura7jacksonazm', 'z3unwRwMaV'),
-#     ('karen0perezdeb', 'kRexmitDi'),
-#     ('donna2thomaszcc', 'MqbfPZnG8pZ'),
-# ]
-
-# for insta in instas:
-#     print(f"{insta[0]},{insta[1]},,,,")
-# start = 0
-# for i in range(start, len(instas)):
-#     print(instas[i])
-# instas['Country'][3] = 'USA'
-# print(instas)
-# save_instas()
-
-
-# print('alix_earle_highlights'.split('_')[-1])
-# print('aaaa'.index('b'))
-
-save_filedata('data/tiktok_accounts_data.txt', dict())
 prev_data = open_filedata('data/tiktok_accounts_data.txt')
 
 pp = pprint.PrettyPrinter(depth=6)
