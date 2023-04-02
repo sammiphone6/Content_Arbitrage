6f2c99124151c76239912ce59318e40b91c91660
Sam <sammit@mit.edu>
2023-04-02 17:42:33 -0400

    made speed_up function in experiment.py

--- account_adding/experiment.py
+++ account_adding/experiment.py
@@ -28,3 +28,49 @@ import pandas.io.clipboard as pic
 
 # pic.clipboard_set('gerer')
 # print(pic.clipboard_get())
+
+
+def speed_up(web_hosted_tt_link, speed):
+    # 0.5 < speed < 2
+
+    response1 = requests.get('https://ezgif.com/video-speed?url='+web_hosted_tt_link)
+    text1 = response1.text
+
+    link1 = text1.split('.mp4')[0].split("\"")[-1]+'.mp4'
+
+    headers = {
+        'authority': 'ezgif.com',
+        'accept': '*/*',
+        'accept-language': 'en-US,en;q=0.9',
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'origin': 'https://ezgif.com',
+        'referer': 'https://ezgif.com/video-speed/ezgif-1-47fe12ce2a.mp4',
+        'sec-ch-ua': '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
+        'x-requested-with': 'XMLHttpRequest',
+    }
+
+    params = {
+        'ajax': 'true',
+    }
+
+    data = {
+        'file': link1.split('/')[-1],
+        'multiplier': speed,
+        'apply_audio': 'on',
+    }
+    response2 = requests.post(f'https://ezgif.com/video-speed/{link1}', params=params, headers=headers, data=data)
+    text2 = response2.text
+
+    link2 = "im" + text2.split('.mp4')[0].split("src=\"//im")[-1]+'.mp4'
+    return link2
+
+
+tt_link = "https://v19.tiktokcdn-us.com/739320ac7d3f2046c90592277527dbc1/642a4a46/video/tos/useast5/tos-useast5-ve-0068c003-tx/2a448767afc042d584af3bd35e017ff3/?a=1233&ch=0&cr=0&dr=0&cd=0%7C0%7C0%7C0&cv=1&br=3590&bt=1795&cs=0&ds=6&ft=kJrRfy7oZ-10PD1Kf05Xg9wdbNJ5vEeC~&mime_type=video_mp4&qs=0&rc=NjxoOGRpZDs7O2g2ZWc3NUBpajNtczM6ZnNvaTMzZzczNEBjMC4uXmItNS8xMV8uY15gYSNrazJhcjQwLmNgLS1kMS9zcw%3D%3D&l=2023040221384107E48AB50AF470FE7DDE"
+speed = 1.16
+print(speed_up(tt_link, speed))
