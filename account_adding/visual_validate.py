32099c1f82df5fa5781911825f245f79029236b5
Sam <sammit@mit.edu>
2023-04-02 19:45:53 +0300

    commits

--- account_adding/visual_validate.py
+++ account_adding/visual_validate.py
@@ -1,17 +1,21 @@
 import shutil
 from data import instas, tiktok_account_data
 import os
+import time
+
 
 ## First remove all files
 subfolder = "aaa_visual_validation"
 for file in os.listdir(f'insta_screenshots/{subfolder}'):
-    os.remove(file)
+    os.remove(f"insta_screenshots/{subfolder}/{file}")
+time.sleep(2)
+
 
 ## Then add all files
 count = 1
 for i in range(len(instas)):
     orig_file = instas['Screenshot'][i]
-    print(i, instas['Result'][i], orig_file)
+    # print(i, instas['Result'][i], orig_file)
     if instas['Result'][i] is True and len(str(orig_file)) > 6:
         orig_parts = orig_file.split('/')
         new_parts = [orig_parts[0], subfolder, f"{count}_{tiktok_account_data[instas['Tiktok username'][i]]['ig_username']}_{orig_parts[1]}"]
@@ -21,6 +25,7 @@ for i in range(len(instas)):
 
         count += 1
 
+
 ## Then calculate the success rate so far
-total = max([i for i in range(len(instas)) if len(str(orig_file)) in [True, False]])+1
+total = max([i for i in range(len(instas)) if instas['Result'][i] in [True, False]])+1
 print('Successful: ', count, '\tTotal: ', total, '\tRatio: ', round(count/total, 2))
