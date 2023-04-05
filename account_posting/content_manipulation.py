4b6b50a166b1fe1cac6ca24d362160644f6208b9
Sam <sammit@mit.edu>
2023-04-05 12:41:42 -0400

    commits

--- account_posting/content_manipulation.py
+++ account_posting/content_manipulation.py
@@ -91,6 +91,48 @@ def tiktok_to_download_url2(tiktok_link): #savetik.co
 def tiktok_to_download_url(tiktok_link):
     return tiktok_to_download_url2(tiktok_link)
 
+def download_file(download_link, downloaded_file_name):
+    chunk_size = 256
+    url = download_link
+
+    r = requests.get(url, stream = True)
+
+    with open(downloaded_file_name, "wb") as f:
+        for chunk in r.iter_content(chunk_size=chunk_size):
+            f.write(chunk)
+
+def host_file_online(downloaded_file_name):
+    files= {
+        'reqtype': (None, 'fileupload'),
+        'time': (None, '1h'),
+        'fileToUpload': open(downloaded_file_name, 'rb'),
+    }
+    response = requests.post('https://litterbox.catbox.moe/resources/internals/api.php', files=files)
+    return response.text
+    
+def tiktok_to_webhosted_link_old(tiktok_link):
+    print(1)
+    download_link = tiktok_to_download_url(tiktok_link)
+    print(2)
+    if len(str(download_link)) < 5:
+        print(tiktok_link, ' gave download link: ', download_link)
+        return 0
+    rand = str(int(100000*random.random()))
+    print(3)
+
+    directory = 'temp_vids'
+    download_file_name = os.path.join(directory, f"temporary{tiktok_link[-6:-1]}--{rand}.mp4")
+    print(4)
+    download_file(download_link, download_file_name)
+    print(5)
+    webhosted_link = host_file_online(download_file_name)
+    print(6)
+    os.remove(download_file_name)
+
+    return webhosted_link
+
+### ONLY FUNCTIONS STILL USED (the rest are old and less efficient)
+
 def tiktok_to_video_data(tiktok_link): #savetik.co
     cookies = {
         '.AspNetCore.Antiforgery.vmpBg8YRfdE': 'CfDJ8F3EzyHODftOuDVASknfNW82Icr5reXHjD2FRVwA-OvMwNEWKdsem7A8Xi8id6m_dXOOWtf0WfaSsZSagF1BVkIY6rau7AMgGf14ad7WyIcfoZTbdVJBB5IW9xMvWcvLqJfN8kPaUf3qRHvbMQp_uQE',
@@ -132,46 +174,6 @@ def tiktok_to_video_data(tiktok_link): #savetik.co
         return video_data
     except:
         return 0
-
-def download_file(download_link, downloaded_file_name):
-    chunk_size = 256
-    url = download_link
-
-    r = requests.get(url, stream = True)
-
-    with open(downloaded_file_name, "wb") as f:
-        for chunk in r.iter_content(chunk_size=chunk_size):
-            f.write(chunk)
-
-def host_file_online(downloaded_file_name):
-    files= {
-        'reqtype': (None, 'fileupload'),
-        'time': (None, '1h'),
-        'fileToUpload': open(downloaded_file_name, 'rb'),
-    }
-    response = requests.post('https://litterbox.catbox.moe/resources/internals/api.php', files=files)
-    return response.text
-    
-def tiktok_to_webhosted_link_old(tiktok_link):
-    print(1)
-    download_link = tiktok_to_download_url(tiktok_link)
-    print(2)
-    if len(str(download_link)) < 5:
-        print(tiktok_link, ' gave download link: ', download_link)
-        return 0
-    rand = str(int(100000*random.random()))
-    print(3)
-
-    directory = 'temp_vids'
-    download_file_name = os.path.join(directory, f"temporary{tiktok_link[-6:-1]}--{rand}.mp4")
-    print(4)
-    download_file(download_link, download_file_name)
-    print(5)
-    webhosted_link = host_file_online(download_file_name)
-    print(6)
-    os.remove(download_file_name)
-
-    return webhosted_link
 
 def speed_up(web_hosted_tt_link, speed): # 0.5 < speed < 2
     debug = False
@@ -221,26 +223,6 @@ def tiktok_to_webhosted_link(tiktok_link, speed = None):
         return video_data
     else:
         return speed_up(video_data, float(speed))
-        
 
-# this link is missing though
-# tt_link = "https://v19.tiktokcdn-us.com/739320ac7d3f2046c90592277527dbc1/642a4a46/video/tos/useast5/tos-useast5-ve-0068c003-tx/2a448767afc042d584af3bd35e017ff3/?a=1233&ch=0&cr=0&dr=0&cd=0%7C0%7C0%7C0&cv=1&br=3590&bt=1795&cs=0&ds=6&ft=kJrRfy7oZ-10PD1Kf05Xg9wdbNJ5vEeC~&mime_type=video_mp4&qs=0&rc=NjxoOGRpZDs7O2g2ZWc3NUBpajNtczM6ZnNvaTMzZzczNEBjMC4uXmItNS8xMV8uY15gYSNrazJhcjQwLmNgLS1kMS9zcw%3D%3D&l=2023040221384107E48AB50AF470FE7DDE"
-# speed = 1.16
 
-# links = [
-#     'https://www.tiktok.com/@burnt_pellet_bbq/video/7046058551474687237',
-#     # 'https://www.tiktok.com/@tyrecordslol/video/7203020961937984814',
-#     # 'https://www.tiktok.com/@holliskitten/video/7176299886608911658',
-#     # 'https://www.tiktok.com/@dunk_fmvp/video/7172582923382672683',
-#     # 'https://www.tiktok.com/@kirill_mist/video/7131948387317959982',
-#     # 'https://www.tiktok.com/@sophiebouts/video/7118489020610530565',
-# ]
-# start = time.time()
-# for link in links:
-#     print("\nLINK STARTING: ", link)
 
-#     video_data = tiktok_to_video_data(link)
-#     print('download url', video_data)
-#     speed_url = speed_up(video_data, speed)
-#     print('url', speed_url)
-#     print(time.time()-start)
