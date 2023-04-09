5fc0c2419c2a821930208cacacd9da0ca5462eb3
Sam <sammit@mit.edu>
2023-04-09 17:26:12 -0400

    commits

--- fsm_main.py
+++ fsm_main.py
@@ -23,34 +23,35 @@ def update():
         for facebook_account in fbs.index:
             if len(str(fbs['Access Token'][facebook_account])) > 8 and facebook_account not in fb_app_data.index:
                 new_row = dict()
-                new_row['Email'] = facebook_account
-                new_row['App ID'] = fbs['App ID'][facebook_account]
-                new_row['App Secret'] = fbs['App Secret'][facebook_account]
-                new_row['Access Token'] = fbs['Access Token'][facebook_account]
+                new_row['Email'] = [facebook_account]
+                new_row['App ID'] = [fbs['App ID'][facebook_account]]
+                new_row['App Secret'] = [fbs['App Secret'][facebook_account]]
+                new_row['Access Token'] = [fbs['Access Token'][facebook_account]]
 
                 print(fb_app_data)
-                fb_app_data = fb_app_data.append(pd.DataFrame(new_row, index=new_row['Email'], columns=fb_app_data.columns))#new_row, index = ['Email'], columns = fb_app_data.columns)
+                fb_app_data = fb_app_data.reset_index().append(pd.DataFrame(new_row)).set_index(['Email'])
                 print(fb_app_data)
-                save_fb_app_data()
+                # save_fb_app_data()
 
                 facebook_added = True
         
         for i in range(len(instas)):
             insta = instas.iloc[i]
-            if insta['Facebook Result'] == True and insta['Tiktok username'] not in account_data_indiv.index and insta['Facebook account'] in fbs.index:
+            if insta['Facebook Result'] == True and insta['Tiktok username'] not in account_data_indiv.index and insta['Facebook account'] in fb_app_data.index:
                 new_row = dict()
                 new_row['TT Account'] = insta['Tiktok username']
-                new_row['IG ID'] = get_instagram_id(fb_app_data.loc[fb_app_data['Email'] == insta['Facebook account'], 'Access Token'].iloc[0,0], insta['Page name'])
+                print()
+                new_row['IG ID'] = get_instagram_id(fb_app_data['Access Token'][insta['Facebook account']], insta['Page name'])
                 new_row['FB App Owner'] = insta['Facebook account']
                 new_row['Hashtag'] = insta['Tiktok username']
                 new_row['Instagram'] = tiktok_account_data[insta['Tiktok username']]['ig_username']
 
                 account_data_indiv = account_data_indiv.append(new_row, ignore_index = True)
-                save_account_data_indiv()
+                # save_account_data_indiv()
 
         ## if facebook_added then get long lived access token
-        if facebook_added:
-            update_all_access_tokens()
+        # if facebook_added:
+        #     update_all_access_tokens()
 
         time.sleep(60*15)
 
@@ -64,8 +65,8 @@ def update():
 
 
 types = [
-    'insta',
-    # 'facebook',
+    # 'insta',
+    'facebook',
 ]
 
 if 'insta' in types:
@@ -74,6 +75,6 @@ if 'insta' in types:
 
 if 'facebook' in types: 
     while True: 
-        facebook_pairing_script()
-        update()
+        # facebook_pairing_script()
+        # update()
 
