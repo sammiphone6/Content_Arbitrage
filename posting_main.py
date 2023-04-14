a5d97cf402a8258b1a241ef38d3e88b1c8247c26
Sam <sammit@mit.edu>
2023-04-14 15:30:27 -0400

    commits

--- posting_main.py
+++ posting_main.py
@@ -24,17 +24,17 @@ def post_all(post_types):
 
 ## Just comment out whichever one you don't want
 post_types = [
-    'indiv',
-    'popular',
+    # 'indiv',
+    # 'popular',
 ]
     
-# posts_sync(list({'dayynaa', 'kevwithin', 'cooking'}))
+# posts_sync(list({'dayynaa', 'cooking', 'kevwithin'}))
 # update_all_access_tokens()
 # debug_all_access_tokens()
 
 run_tests(deep_test=False)
 get_insights()
-# post_all(post_types)
+post_all(post_types)
 plot_barchart() #per day
 plot_barchart(days=30, log_scale=True, cumulative=True) #per acct
 
